0b3b3ff6d4f544ff875e7ac0e906916f2192e46c
amsuu <102627400+amsuu@users.noreply.github.com>
2023-12-17 15:55:47 +0200

    Make site remember chosen theme #2

---
+++ src/app/funcs.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class FuncsService {
+  constructor() { }
+
+  getCurrentTheme = () => localStorage.getItem('theme') === 'dark' ? 'dark' : 'light';
+
+  toggleTheme() {
+
+    let currentTheme = document.body.getAttribute('theme') || 'light';
+    let newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    // setTheme() here because angular is being stupid
+    document.body.setAttribute('theme', newTheme);
+    localStorage.setItem('theme', newTheme);
+  }
+
+
+  getOppositeTheme = (theme: string) => theme === 'light' ? 'dark' : 'light';
+}
--- src/app/theme-selector/theme-selector.component.ts
+++ src/app/theme-selector/theme-selector.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { FuncsService } from '../funcs.service';
 
 @Component({
   selector: 'app-theme-selector',
@@ -6,19 +7,42 @@ import { AfterViewInit, Component, ElementRef } from '@angular/core';
   styleUrls: ['./theme-selector.component.scss']
 })
 export class ThemeSelectorComponent {
-  constructor(private elementRef:ElementRef) {}
+  constructor(private elementRef: ElementRef, private funcs: FuncsService) {}
 
   ngAfterViewInit() {
+
+    let currentTheme = this.funcs.getCurrentTheme() === 'dark' ? 'dark' : 'light';
+
+    this.toggleThemeHandler();
+
+    if (currentTheme === 'light' || !currentTheme) {
+      this.toggleThemeHandler();
+    }
+
     this.elementRef.nativeElement.querySelector('#theme-selection-light')
-    .addEventListener('click', this.toggleTheme.bind(this));
+    .addEventListener('click', this.toggleThemeHandler.bind(this));
     this.elementRef.nativeElement.querySelector('#theme-selection-dark')
-    .addEventListener('click', this.toggleTheme.bind(this));
+    .addEventListener('click', this.toggleThemeHandler.bind(this));
   }
-  
-  toggleTheme() {
-    let currentTheme = document.body.getAttribute('theme') || 'light';
-    document.body.setAttribute('theme', currentTheme === 'light' ? 'dark' : 'light');
-    this.elementRef.nativeElement.querySelector('#theme-selection-light').classList.toggle('active-theme');
-    this.elementRef.nativeElement.querySelector('#theme-selection-dark').classList.toggle('active-theme');
+
+  toggleThemeHandler() {
+    
+    this.funcs.toggleTheme();
+
+    let newTheme = localStorage.getItem('theme');
+
+    // extra condition at the end because of types
+    this.updateThemeSelector(newTheme === 'light' ? 'light' : 'dark');
+  }
+
+  updateThemeSelector(theme: 'light' | 'dark' = this.funcs.getCurrentTheme()) {
+
+    this.elementRef.nativeElement
+    .querySelector(`#theme-selection-${theme}`)
+    .classList.add('active-theme');
+
+    this.elementRef.nativeElement
+    .querySelector(`#theme-selection-${this.funcs.getOppositeTheme(theme)}`)
+    .classList.remove('active-theme');
   }
 }
