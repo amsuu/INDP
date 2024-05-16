6ccebc5f0c226af4bd9093de079d3de44c4ebf6c
amsuu <102627400+amsuu@users.noreply.github.com>
2024-05-17 02:20:40 +0300

    Various small stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { SettingsComponent } from './settings/settings.component'
 import { HelpPageComponent } from './help-page/help-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'settings', component: SettingsComponent },
   { path: 'help', component: HelpPageComponent}
 ];
