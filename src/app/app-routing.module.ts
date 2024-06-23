72bb149f4e871018fc23f1250728f04f8f947aff
amsuu <102627400+amsuu@users.noreply.github.com>
2024-06-23 05:03:18 +0300

    basic resizing and things for adaptive layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,16 @@ import { SettingsComponent } from './settings/settings.component'
 import { LearnPageComponent } from './learn-page/learn-page.component';
 import { HomeComponent } from './home/home.component';
 import { Level2PageComponent } from './level-2-page/level-2-page.component';
+import { TheoryLearnPageComponent } from './theory-learn-page/theory-learn-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'level-1', component: Level1pageComponent },
-  { path: 'level-2', component: Level2PageComponent },
+  { path: 'level/1', component: Level1pageComponent },
+  { path: 'level/2', component: Level2PageComponent },
   { path: 'settings', component: SettingsComponent },
-  { path: 'learn', component: LearnPageComponent}
+  { path: 'learn', component: LearnPageComponent },
+  { path: 'learn/theory', component: TheoryLearnPageComponent }
 ];
 
 @NgModule({
