9b6851b9d535f4c1554de10ad2208d58380070c3
amsuu <102627400+amsuu@users.noreply.github.com>
2024-07-10 15:24:33 +0300

    space

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes, { useHash: true})],
+  imports: [CommonModule, RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 
