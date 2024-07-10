9d470a36630041343c93ad7bbd81ca3038b7e09d
amsuu <102627400+amsuu@users.noreply.github.com>
2024-07-10 14:11:16 +0300

    resolve Links to any page other than root (/INDP) return 404 #32

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes, { useHash: true})],
   exports: [RouterModule]
 })
 
