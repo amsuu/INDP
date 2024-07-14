12e12042264d3d5bdc7eb7474a688bc9820cc980
amsuu <102627400+amsuu@users.noreply.github.com>
2024-07-14 19:28:49 +0300

    prob not planning this, but At least i fixed my npm

--- src/app/level-2-page/level-2-page.component.ts
+++ src/app/level-2-page/level-2-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Level2QuizQuestionComponent } from '../level-2-quiz-question/level-2-quiz-question.component';
 
 @Component({
   selector: 'app-level-2-page',
