30b44a7645e95a8c5ea80aaa2b1af6ef1caf7d88
amsuu <102627400+amsuu@users.noreply.github.com>
2024-08-11 01:36:14 +0300

    level 2 design and fixing level 1 z index

--- src/app/level-2-quiz-question/level-2-quiz-question.component.ts
+++ src/app/level-2-quiz-question/level-2-quiz-question.component.ts
@@ -11,25 +11,47 @@ export class Level2QuizQuestionComponent {
    * The phrase, where each time the string is broken up into
    * different elements of the array, that represents a gap
    * in the UI, where the correct form is wanted.
+   * An empty string means a place for an input.
    */
-  @Input() phrase = [''];
+  @Input() phrase = ['Ja vidžų', '', '', '.'];
   /**
    * These are the placeholders (the pre-text that disappears when you
    * type something) of the input elements in the blank spots of the
    * phrase.
    * Analagous to Level-1's "root words"
    */
-  @Input() placeholders = [''];
+  @Input() placeholders = ['dobry', 'mųž'];
   /**
    * These are the correct forms of the placeholder words.
    * ! (when inflection by code is implimented, this should change too)
    * The length must be the same as placeholders.length.
    */
-  @Input() correctAnswers = [''];
+  @Input() correctAnswers = ['dobrogo', 'mųža'];
+
+  phraseToPlaceholderMap: number[] = [ ]
 
   constructor() {
 
+    // the following initiates the phraseToPlaceholderMap element.
+    // the idea is to make a map between the indexes of the
+    // elements in "phrase" which are empty (stand-ins for
+    // the input fields in the final question) and the indexes
+    // of the elements in the arrays of "placeholders" and
+    // "correctAnswers" since these are inherintly out-of-sync
+
+    let tracker = 0;
+    for (let i = 0; i < this.phrase.length; i++) {
+      let phraseEl = this.phrase[i];
+      if (phraseEl === '') {
+        this.phraseToPlaceholderMap[i] = tracker;
+        tracker++;
+      }
+    }
   }
 
+
+  getPlaceholderFromPhrase(i: number) {
+    return this.placeholders[this.phraseToPlaceholderMap[i]];
+  }
 
 }
