214b5a154f4104ecf2ceb615bc047e9626b7cc89
amsuu <102627400+amsuu@users.noreply.github.com>
2024-09-07 05:05:50 +0300

    theme-selector plus confusion

--- src/app/theme-selector/theme-selector.component.ts
+++ src/app/theme-selector/theme-selector.component.ts
@@ -9,27 +9,31 @@ import { ThemeService } from '../theme.service';
 export class ThemeSelectorComponent {
   constructor(private elementRef: ElementRef, private themeService: ThemeService) {}
 
+  // get theme from local storage if possible
+  // add event listeners
   ngOnInit() {
     this.updateThemeSelector();
 
     this.elementRef.nativeElement.querySelector('.theme-selector')
     .addEventListener('click', this.toggleThemeHandler.bind(this));
-    // this.elementRef.nativeElement.querySelector('#theme-selection-light')
-    // .addEventListener('click', this.toggleThemeHandler.bind(this));
-    // this.elementRef.nativeElement.querySelector('#theme-selection-dark')
-    // .addEventListener('click', this.toggleThemeHandler.bind(this));
   }
 
+  // Event listener to the buttons.
+  // Changes the theme in UI
+  // AND in local storage
   toggleThemeHandler() {
 
+    // change theme in local storage
     this.themeService.toggleTheme();
 
+    // make sure you are setting whatever is in the local storage
     let newTheme = localStorage.getItem('theme');
 
-    // extra condition at the end because of types
+    // change the UI (extra condition at the end because of types)
     this.updateThemeSelector(newTheme === 'light' ? 'light' : 'dark');
   }
 
+  // updates classes to show the correct theme visually
   updateThemeSelector(theme: 'light' | 'dark' = this.themeService.getCurrentThemeName()) {
 
     this.elementRef.nativeElement
