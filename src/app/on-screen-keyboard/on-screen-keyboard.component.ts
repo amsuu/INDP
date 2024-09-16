448ffcfee54487287a5428caaf31568079191e3c
amsuu <102627400+amsuu@users.noreply.github.com>
2024-09-17 00:11:39 +0300

    i forgor to add

---
+++ src/app/on-screen-keyboard/on-screen-keyboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-on-screen-keyboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './on-screen-keyboard.component.html',
+  styleUrl: './on-screen-keyboard.component.scss'
+})
+export class OnScreenKeyboardComponent {
+
+}
