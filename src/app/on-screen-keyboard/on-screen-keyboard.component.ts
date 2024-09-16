5eaa8c44f4b70f672dd817307a72bb188ed1cd25
amsuu <102627400+amsuu@users.noreply.github.com>
2024-09-17 02:09:20 +0300

    functionality

--- src/app/on-screen-keyboard/on-screen-keyboard.component.ts
+++ src/app/on-screen-keyboard/on-screen-keyboard.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-on-screen-keyboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './on-screen-keyboard.component.html',
   styleUrl: './on-screen-keyboard.component.scss'
 })
 export class OnScreenKeyboardComponent {
 
+  public keepOriginalOrder = (a: any, b: any) => a.key
+
+  orthographies = {
+    "Standard Orthography": [
+      ['č', 'š', 'ž', 'ě'],
+    ],
+    "Etymological Orthography": [
+      ['å', 'ę', 'ų', 'ė', 'ȯ', 'ć', 'đ'],
+      ['ĺ', 'ń', 'ŕ', 'ť', 'ď', 'ś', 'ź'],
+    ],
+  }
+
+  write(value: string) {
+    const destination = document.activeElement as HTMLInputElement;
+    if (destination.tagName === 'INPUT') {
+      destination.value += value;
+    }
+  }
 }
