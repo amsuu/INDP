4699068b2ddceb34cef08e3d25d6055cc243acd6
amsuu <102627400+amsuu@users.noreply.github.com>
2024-09-24 09:40:20 +0300

    http working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
