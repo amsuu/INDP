2d982cb8c83ddfcd74199e12fbdb63e4e236ef7f
amsuu <102627400+amsuu@users.noreply.github.com>
2024-11-01 02:03:09 +0200

    a lot

---
+++ src/app/_learn/theory/glossary/glossary/glossary.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { _Component } from './glossary.component';
+
+describe('_Component', () => {
+  let component: _Component;
+  let fixture: ComponentFixture<_Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [_Component]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(_Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/_learn/theory/index.ts
+++ src/app/_learn/theory/index.ts
@@ -1,3 +1,4 @@
 export  * as _Cases  from './cases';
 export  * as _ISV  from './interslavic';
 export  * as _Writing  from './writing';
+export  * as _Glossary  from './glossary';
