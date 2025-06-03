d9aa312041764e8ed26349331a845541508b1b8f
amsuu <102627400+amsuu@users.noreply.github.com>
2025-06-03 03:10:18 +0300

    restructureing and renaming learn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { Level1pageComponent } from './level-1-page/level-1-page.component'
 import { SettingsComponent } from './settings/settings.component'
-import { LearnPageComponent } from './learn/learn-page/learn-page.component';
+import { LearnComponent } from './learn/learn.component';
 import { HomeComponent } from './home/home.component';
 import { Level2PageComponent } from './level-2-page/level-2-page.component';
 import { LearnRoutingService } from "./learn/learn-routing.service";
@@ -13,6 +13,6 @@ export const routes: Routes = [
   { path: 'level/1', component: Level1pageComponent },
   { path: 'level/2', component: Level2PageComponent },
   { path: 'settings', component: SettingsComponent },
-  { path: 'learn', component: LearnPageComponent },
+  { path: 'learn', component: LearnComponent },
   ...routingService.formatPathsForRoutesTS(routingService.CompiledPaths)
 ];
---
+++ src/app/learn/learn.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { LevelSelectCardsComponent } from '../level-select-cards/level-select-cards.component';
+
+@Component({
+    selector: 'app-learn-page',
+    templateUrl: './learn.component.html',
+    styleUrls: ['./learn.component.scss'],
+    standalone: true,
+    imports: [LevelSelectCardsComponent]
+})
+export class LearnComponent {
+
+}
