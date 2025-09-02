82d944935e897c524cf7f911fed8c7f00d5e2ff7
amsuu <102627400+amsuu@users.noreply.github.com>
2025-09-03 00:45:56 +0300

    basic quiz generation and visualization working

--- src/app/levels/level-1-creator/level-1-creator.component.ts
+++ src/app/levels/level-1-creator/level-1-creator.component.ts
@@ -1,36 +1,53 @@
 import { Component } from '@angular/core';
-import { QuestionFactory, Quiz } from '../level-1-types';
+import { Declension, Disambig, QuestionFactory, Quiz } from '../level-1-types';
 import { AzService } from '../../az/az.service';
+import { NgFor } from '@angular/common';
+import { Level1QuizQuestionComponent } from '../level-1-quiz-question/level-1-quiz-question.component';
 
 @Component({
   selector: 'app-level-1-creator',
   standalone: true,
-  imports: [],
+  imports: [NgFor, Level1QuizQuestionComponent],
   templateUrl: './level-1-creator.component.html',
   styleUrl: './level-1-creator.component.scss'
 })
 export class Level1CreatorComponent {
+  quiz: Quiz = {
+    author: "unknown",
+    title: "unknown",
+    questions: []
+  };
 
   constructor(private azS: AzService) { }
 
-  cl() {
+  add(word: string,
+     disambigPoS: HTMLSelectElement,
+     disambigGen: HTMLSelectElement,
+     disambigNum: HTMLSelectElement,
 
-    this.azS.loadThen(az => {
-      let quiz: Quiz = {
-        author: "amsuu",
-        title: "test",
-        questions: [
+     targetCase: HTMLSelectElement,
+     targetGen: HTMLSelectElement,
+     targetNum: HTMLSelectElement,
+   ) {
 
-        ]
-      }
+     let disambig: Disambig = { case: 'nomn', }
+     if (disambigPoS.value === '') disambig.PoS    = disambigPoS.value;
+     if (disambigGen.value === '') disambig.gender = disambigGen.value;
+     if (disambigNum.value === '') disambig.number = disambigNum.value;
 
-      let x = QuestionFactory(az, 'brat', {
-        disambig: { PoS: 'NOUN', case: 'nomn', number: 'sing' },
-        target: { CAse: 'datv', NMbr: 'sing' }
-      })
-      if (x) quiz.questions.push(x);
+     let target: Declension = {
+       CAse: targetCase.value,
+       NMbr: targetNum.value,
+     };
+     if (targetGen.value !== '') target.GNdr = targetGen.value
 
-      console.log(quiz);
-    });
-  }
+     this.azS.loadThen(az => {
+       let x = QuestionFactory(az, word, { disambig, target, });
+
+       if (x) this.quiz.questions.push(x);
+       else console.error('failed');
+
+       console.log(this.quiz);
+     });
+   }
 }
