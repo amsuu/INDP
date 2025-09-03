c79e1953003abf85a1ad3adc3b0ff0e8a77074cb
amsuu <102627400+amsuu@users.noreply.github.com>
2025-09-03 15:00:14 +0300

    full export and import working

--- src/app/levels/level-1-page/level-1-page.component.ts
+++ src/app/levels/level-1-page/level-1-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, input, OnInit } from '@angular/core';
-import { NgFor } from "@angular/common";
+import { NgFor, NgIf } from "@angular/common";
 import { ButtonsOverlayComponent } from '../buttons-overlay/buttons-overlay.component';
 import { Level1QuizQuestionComponent } from '../level-1-quiz-question/level-1-quiz-question.component';
 import { Quiz } from '../level-1-types';
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
     templateUrl: './level-1-page.component.html',
     styleUrls: ['./level-1-page.component.scss'],
     standalone: true,
-    imports: [ButtonsOverlayComponent, NgFor, Level1QuizQuestionComponent]
+    imports: [ButtonsOverlayComponent, NgIf, NgFor, Level1QuizQuestionComponent]
 })
 export class Level1pageComponent implements OnInit {
 
@@ -25,11 +25,30 @@ export class Level1pageComponent implements OnInit {
   len = 0;
 
   ngOnInit() {
-    this.http.get<Quiz>('/amsuu_l1-example-quiz.json').subscribe(json => {
+    this.http.get<Quiz>('/amsuu_level-1-example-quiz.json').subscribe(json => {
       this.quiz = json;
       this.len = this.quiz.questions.length;
       console.log(this.quiz);
     });
+  }
+
+  import(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files || input.files.length === 0) return;
+
+    const file = input.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      try {
+        this.quiz = JSON.parse(reader.result as string);
+      } catch (err) {
+        console.error('Invalid JSON file:', err);
+        alert('Invalid JSON file');
+      }
+    };
+
+    reader.readAsText(file);
   }
 }
 
