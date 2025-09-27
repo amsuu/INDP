c738b925465a881ea47cdd612f4c90cef7c9af14
amsuu <102627400+amsuu@users.noreply.github.com>
2025-09-27 15:55:48 +0300

    port over everything useful. BTW i know i should be more DRY, but this will be done later, once i know what will actually be common across everything and what won't

--- src/app/levels/level-2-creator/level-2-creator.component.ts
+++ src/app/levels/level-2-creator/level-2-creator.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import { Quiz, QuestionFactory } from '../level-2-types';
+import { NgFor, NgIf } from '@angular/common';
+import { Level2QuizQuestionComponent } from '../level-2-quiz-question/level-2-quiz-question.component';
+import { SegmentedSingleSelectionComponent } from '../../settings/segmented-single-selection/segmented-single-selection.component';
+import { AzService } from '../../az/az.service';
+import { Declension, Disambig } from '../level-types';
+import { cases, numbers } from '../../az/utils';
+
+const disgen = ['', 'masc', 'femn', 'neut'];
+const tarnum = [...numbers];
+const tarcas = [...cases];
 
 @Component({
   selector: 'app-level-2-creator',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, Level2QuizQuestionComponent, SegmentedSingleSelectionComponent],
   templateUrl: './level-2-creator.component.html',
   styleUrl: './level-2-creator.component.scss'
 })
 export class Level2CreatorComponent {
+  protected disgenSelection = 0;
+  protected tarnumSelection = 0;
+  protected tarcasSelection = 0;
 
+  quiz: Quiz = {
+    author: "unknown",
+    title: "unknown",
+    questions: []
+  };
+
+
+  constructor(private azS: AzService) { }
+
+  addElement() {
+
+  }
+
+  addQuestion() {
+
+  }
+
+  export(author: HTMLInputElement, title: HTMLInputElement) {
+
+    // 1. validate
+    if (author.value !== '') this.quiz.author = author.value;
+    if (title.value !== '') this.quiz.title = title.value;
+
+    // 2. download (i copied this code from somewhere)
+    // prepare download
+    const json = JSON.stringify(this.quiz);
+    const blob = new Blob([json], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    // start download
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${this.quiz.author}_${this.quiz.title}.json`;
+    a.click();
+
+    // finish download
+    URL.revokeObjectURL(url);
+  }
 }
