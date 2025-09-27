aa85714fcf53ef880370af74b119d0ec5773fe38
amsuu <102627400+amsuu@users.noreply.github.com>
2025-09-27 15:47:53 +0300

    create creator page

---
+++ src/app/levels/level-2-creator/level-2-creator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-level-2-creator',
+  standalone: true,
+  imports: [],
+  templateUrl: './level-2-creator.component.html',
+  styleUrl: './level-2-creator.component.scss'
+})
+export class Level2CreatorComponent {
+
+}
