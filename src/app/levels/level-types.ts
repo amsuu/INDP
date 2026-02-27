02a4c1ab599584f89693bd6bdb62f287a72f67af
amsuu <102627400+amsuu@users.noreply.github.com>
2026-02-27 13:03:12 +0200

    add to my types. Simply adding it to the declensor doesn't work so I'll have to figure something more out

--- src/app/levels/level-types.ts
+++ src/app/levels/level-types.ts
@@ -3,11 +3,13 @@ export type Disambig = {
   gender?: string,
   number?: string,
   case?: string,
+  animacy?: string,
 };
 export type Declension = {
     CAse: string,
     NMbr: string,
     GNdr?: string,  // only for adj.
+    Animacy?: string,
 };
 export type DeclensedWord = {
   word: string;
